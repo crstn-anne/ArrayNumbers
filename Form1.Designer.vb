436893d246769120aefe26bb494fa8ac151a4edc
Cristine Gacos <crstn.anne27@outlook.com>
2023-10-04 21:08:07 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btn_input_count = New Button()
+        lb_input_count = New Label()
+        btn_add = New Button()
+        btn_inputs_arr = New Button()
+        btn_getLowest = New Button()
+        btn_hightest = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' btn_input_count
+        ' 
+        btn_input_count.Location = New Point(41, 36)
+        btn_input_count.Name = "btn_input_count"
+        btn_input_count.Size = New Size(132, 30)
+        btn_input_count.TabIndex = 0
+        btn_input_count.Text = "Enter Input Count"
+        btn_input_count.UseVisualStyleBackColor = True
+        ' 
+        ' lb_input_count
+        ' 
+        lb_input_count.AutoSize = True
+        lb_input_count.Location = New Point(192, 44)
+        lb_input_count.Name = "lb_input_count"
+        lb_input_count.Size = New Size(328, 15)
+        lb_input_count.TabIndex = 1
+        lb_input_count.Text = "How many Inputs you want to add in your array of numbers?"
+        ' 
+        ' btn_add
+        ' 
+        btn_add.Location = New Point(41, 108)
+        btn_add.Name = "btn_add"
+        btn_add.Size = New Size(132, 30)
+        btn_add.TabIndex = 3
+        btn_add.Text = "Get Sum"
+        btn_add.UseVisualStyleBackColor = True
+        ' 
+        ' btn_inputs_arr
+        ' 
+        btn_inputs_arr.Location = New Point(41, 72)
+        btn_inputs_arr.Name = "btn_inputs_arr"
+        btn_inputs_arr.Size = New Size(132, 30)
+        btn_inputs_arr.TabIndex = 4
+        btn_inputs_arr.Text = "Input Numbers"
+        btn_inputs_arr.UseVisualStyleBackColor = True
+        ' 
+        ' btn_getLowest
+        ' 
+        btn_getLowest.Location = New Point(41, 144)
+        btn_getLowest.Name = "btn_getLowest"
+        btn_getLowest.Size = New Size(132, 30)
+        btn_getLowest.TabIndex = 5
+        btn_getLowest.Text = "Get Lowest Number"
+        btn_getLowest.UseVisualStyleBackColor = True
+        ' 
+        ' btn_hightest
+        ' 
+        btn_hightest.Location = New Point(41, 180)
+        btn_hightest.Name = "btn_hightest"
+        btn_hightest.Size = New Size(132, 30)
+        btn_hightest.TabIndex = 6
+        btn_hightest.Text = "Get Highest Number"
+        btn_hightest.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(192, 188)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(164, 15)
+        Label1.TabIndex = 7
+        Label1.Text = "Get Highest value in the Array"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(192, 152)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(158, 15)
+        Label2.TabIndex = 8
+        Label2.Text = "Get Lowest value in the array"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(192, 116)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(184, 15)
+        Label3.TabIndex = 9
+        Label3.Text = "Get Sum of the calues in the array"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(192, 80)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(133, 15)
+        Label4.TabIndex = 10
+        Label4.Text = "Input values in the array"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(549, 257)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(btn_hightest)
+        Controls.Add(btn_getLowest)
+        Controls.Add(btn_inputs_arr)
+        Controls.Add(btn_add)
+        Controls.Add(lb_input_count)
+        Controls.Add(btn_input_count)
+        Name = "Form1"
+        Text = "Array Numbers"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btn_input_count As Button
+    Friend WithEvents lb_input_count As Label
+    Friend WithEvents btn_add As Button
+    Friend WithEvents btn_inputs_arr As Button
+    Friend WithEvents btn_getLowest As Button
+    Friend WithEvents btn_hightest As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,124 @@
+﻿Public Class Form1
+
+    Dim int_input_count As Integer
+    Dim str_input_count As String
+    Dim bool_inputCountCheck As Boolean
+
+    Dim intArr_Numbers() As Integer
+    Dim strArr_Numbers As String
+    Dim bool_arrNumbersCheck As Boolean
+
+    Private Sub btn_input_count_Click(sender As Object, e As EventArgs) Handles btn_input_count.Click
+
+        Do Until bool_inputCountCheck = True
+            str_input_count = InputBox("Please Enter how many inputs you want to enter in the array")
+
+            If IsNumeric(str_input_count) Then
+                int_input_count = str_input_count
+
+                If int_input_count > 0 Then
+                    bool_inputCountCheck = True
+                    MessageBox.Show("You entered " & int_input_count)
+                    btn_input_count.Enabled = False    'automaticall disabled the textbox after you entered a number
+                Else
+                    MessageBox.Show("Please Enter a Valid input!")
+                    bool_inputCountCheck = False
+                End If
+
+            ElseIf Not IsNumeric(str_input_count) Then
+                MessageBox.Show("Please Enter a Valid input!")
+                bool_inputCountCheck = False
+            End If
+
+        Loop
+
+    End Sub
+
+    Private Sub btn_inputs_arr_Click(sender As Object, e As EventArgs) Handles btn_inputs_arr.Click
+
+        If bool_inputCountCheck = False Then
+            MessageBox.Show("Please Enter how many inputs you want to make.")
+        Else
+            ReDim intArr_Numbers(int_input_count)
+
+            For i = 0 To int_input_count - 1
+                bool_arrNumbersCheck = False
+
+                Do Until bool_arrNumbersCheck = True
+                    strArr_Numbers = InputBox("Please Enter Number " & i + 1 & " :")
+
+                    If IsNumeric(strArr_Numbers) Then
+                        intArr_Numbers(i) = strArr_Numbers
+                        bool_arrNumbersCheck = True
+                    Else
+                        MessageBox.Show("Please Enter a NUMBER.")
+                    End If
+
+                Loop
+
+            Next i
+
+            For j = 0 To int_input_count - 1
+                MessageBox.Show("array [ " & j & " ]: " & intArr_Numbers(j))
+            Next j
+
+        End If
+    End Sub
+
+    Private Sub btn_add_Click(sender As Object, e As EventArgs) Handles btn_add.Click
+
+        Dim int_sum As Integer
+
+        If bool_inputCountCheck = True And bool_arrNumbersCheck = True Then
+
+            For i = 0 To int_input_count - 1
+                int_sum += intArr_Numbers(i)
+            Next i
+            MessageBox.Show("Sum: " & int_sum)
+        Else
+            MessageBox.Show("Please Add a number to the Array.")
+        End If
+
+    End Sub
+
+    Private Sub btn_getLowest_Click(sender As Object, e As EventArgs) Handles btn_getLowest.Click
+
+        Dim int_lowest As Integer
+
+        If bool_inputCountCheck = True And bool_arrNumbersCheck = True Then
+
+            int_lowest = intArr_Numbers(0)
+            For i = 1 To int_input_count - 1 Step 1
+                If int_lowest > intArr_Numbers(i) Then
+                    int_lowest = intArr_Numbers(i)
+                End If
+            Next
+
+            MessageBox.Show("Lowest Number: " & int_lowest)
+
+        Else
+            MessageBox.Show("Please Add a number to the Array.")
+        End If
+    End Sub
+
+    Private Sub btn_hightest_Click(sender As Object, e As EventArgs) Handles btn_hightest.Click
+
+        Dim int_highest As Integer
+
+        If bool_inputCountCheck = True And bool_arrNumbersCheck = True Then
+
+            int_highest = intArr_Numbers(0)
+
+            For a = 1 To int_input_count - 1 Step 1
+                If int_highest < intArr_Numbers(a) Then
+                    int_highest = intArr_Numbers(a)
+                End If
+            Next
+            MessageBox.Show("Highest Number: " & int_highest)
+        Else
+            MessageBox.Show("Please Add a number to the Array.")
+        End If
+
+    End Sub
+
+End Class
